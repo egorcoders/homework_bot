cf870550f6cf1e691fcf0cdfd36caf693bf822ff
Egor <egorcoders@gmail.com>
2022-03-20 19:02:37 +0500

    test_5

--- homework.py
+++ homework.py
@@ -12,36 +12,43 @@ load_dotenv()
 
 class ServiceError(Exception):
     """Ошибка отсутствия доступа по заданному эндпойнту."""
+
     pass
 
 
 class NetworkError(Exception):
     """Ошибка отсутствия сети."""
+
     pass
 
 
 class EndpointError(Exception):
     """Ошибка, если эндпойнт не корректен."""
+
     pass
 
 
 class MessageSendingError(Exception):
     """Ошибка отправки сообщения."""
+
     pass
 
 
 class GlobalsError(Exception):
     """Ошибка, если есть пустые глобальные переменные."""
+
     pass
 
 
 class DataTypeError(Exception):
     """Ошибка, если тип данных не dict."""
+
     pass
 
 
 class ResponseFormatError(Exception):
     """Ошибка, если формат response не json."""
+
     pass
 
 
